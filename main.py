7dfdaea6f445ebf68e4a754b9a7d4ed4e92d105c
David <bullatsev@gmail.com>
2022-01-08 14:58:49 +0300

    git init

---
+++ main.py
@@ -0,0 +1,76 @@
+import pygame
+from ball import Ball
+from random import randint
+
+
+pygame.init()
+pygame.time.set_timer(pygame.USEREVENT, 2000)
+f = pygame.font.SysFont('arial', 30)
+
+W, H = 1000, 650
+sc = pygame.display.set_mode((W, H))
+pygame.display.set_caption('STUPID JOCK')
+
+telega = pygame.image.load('images/led_strips_doge.bmp').convert_alpha()
+t_rect = telega.get_rect(centerx=W//2, bottom=H-5)
+
+
+clock = pygame.time.Clock()
+FPS = 60
+game_score = 0
+
+eats_data = ({'path': 'pixil-frame-0.png', 'score': 100},
+              {'path': 'pixil-frame-0.png', 'score': 150},
+              {'path': 'pixil-frame-0.png', 'score': 200})
+
+eats_surf = [pygame.image.load('images/'+data['path']).convert_alpha() for data in eats_data]
+
+eats = pygame.sprite.Group()
+
+
+def createBall(group):
+    indx = randint(0, len(eats_surf)-1)
+    x = randint(20, W-20)
+    speed = randint(1, 4)
+
+    return Ball(x, speed, eats_surf[indx], eats_data[indx]['score'], group)
+
+
+createBall(eats)
+speed = 10
+
+def collideeats():
+    global game_score
+    for ball in eats:
+        if t_rect.collidepoint(ball.rect.center):
+            game_score += ball.score
+            ball.kill()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        elif event.type == pygame.USEREVENT:
+            createBall(eats)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        t_rect.x -= speed
+        if t_rect.x < 0:
+            t_rect.x = 0
+    elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        t_rect.x += speed
+        if t_rect.x > W-t_rect.width:
+            t_rect.x = W-t_rect.width
+
+    sc.fill('white')
+    eats.draw(sc)
+    sc_text = f.render(str(game_score), 1, (94, 138, 14))
+    sc.blit(sc_text, (20, 10))
+    sc.blit(telega, t_rect)
+    pygame.display.update()
+
+    clock.tick(FPS)
+
+    eats.update(H)
+    collideeats()
